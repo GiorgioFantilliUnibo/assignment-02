bb21bb346c25b976645637dc5cc1b41fda14fb11
LorenzoColletta <lorenzo.colletta@studio.unibo.it>
2025-05-08 20:04:49 +0200

    implements a reactive version of Main example

---
+++ src/main/java/pcd/ass02/RecursiveFileOpenIterator.java
@@ -0,0 +1,44 @@
+package pcd.ass02;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class RecursiveFileOpenIterator implements Iterator<File> {
+
+    private Iterator<File> directories;
+    private Iterator<File> files;
+
+    public RecursiveFileOpenIterator(File workplace) {
+        if (workplace.isDirectory()) {
+            files = Stream.concat(List.of(workplace).stream(), Arrays.stream(workplace.listFiles(File::isFile))).iterator();
+            directories = Arrays.stream(workplace.listFiles(File::isDirectory)).iterator();
+        } else {
+            directories = Collections.emptyIterator();
+            files = List.of(workplace).iterator();
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (files.hasNext())
+            return files.hasNext();
+        else {
+            return directories.hasNext();
+        }
+    }
+
+    @Override
+    public File next() {
+        if(files.hasNext()) {
+            return files.next();
+        } else {
+            files = new RecursiveFileOpenIterator(directories.next());
+            return files.next();
+        }
+    }
+
+}
